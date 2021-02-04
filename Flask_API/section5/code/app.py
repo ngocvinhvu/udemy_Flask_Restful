8590620c28d49d64ada58919769c84bc967526ca
vinh <vinhvungoc@vccorp.vn>
2021-02-04 10:16:48 +0700

    add

---
+++ Flask_API/section5/code/app.py
@@ -0,0 +1,22 @@
+from os import name
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from user import UserRegister
+from security import authenticate, identity
+from item import Item, ItemList
+
+
+app = Flask(__name__)
+app.secret_key = 'jose'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
